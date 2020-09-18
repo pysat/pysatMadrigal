5f53f7a155f00a44f3b08ad0a44af2182454a56a
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-09-18 08:30:57 -0400

    DEP: updated deprecated aspects of test functions
    Updated the use of deprecated kwarg `package` and attributes `inst` and `name`.

--- pysatMadrigal/tests/test_instruments.py
+++ pysatMadrigal/tests/test_instruments.py
@@ -4,7 +4,7 @@ import pysatMadrigal
 from pysat.tests.instrument_test_class import generate_instrument_list
 from pysat.tests.instrument_test_class import InstTestClass
 
-instruments = generate_instrument_list(package=pysatMadrigal.instruments)
+instruments = generate_instrument_list(inst_loc=pysatMadrigal.instruments)
 
 method_list = [func for func in dir(InstTestClass)
                if callable(getattr(InstTestClass, func))]
@@ -12,18 +12,20 @@ method_list = [func for func in dir(InstTestClass)
 for method in method_list:
     if hasattr(getattr(InstTestClass, method), 'pytestmark'):
         # Get list of names of pytestmarks
-        Nargs = len(getattr(InstTestClass, method).pytestmark)
-        names = [getattr(InstTestClass, method).pytestmark[j].name
-                 for j in range(0, Nargs)]
+        mark_name = [mod_mark.name for mod_mark
+                     in getattr(InstTestClass, method).pytestmark]
+
         # Add instruments from your library
-        if 'all_inst' in names:
-            mark = pytest.mark.parametrize("name", instruments['names'])
+        if 'all_inst' in mark_name:
+            mark = pytest.mark.parametrize("inst_name", instruments['names'])
             getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'download' in names:
-            mark = pytest.mark.parametrize("inst", instruments['download'])
+        elif 'download' in mark_name:
+            mark = pytest.mark.parametrize("inst_dict",
+                                           instruments['download'])
             getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'no_download' in names:
-            mark = pytest.mark.parametrize("inst", instruments['no_download'])
+        elif 'no_download' in mark_name:
+            mark = pytest.mark.parametrize("inst_dict",
+                                           instruments['no_download'])
             getattr(InstTestClass, method).pytestmark.append(mark)
 
 # remote_file_list not functional in current code.  Disabling for now
@@ -35,9 +37,11 @@ if 'test_remote_file_list' in method_list:
 class TestInstruments(InstTestClass):
 
     def setup(self):
-        """Runs before every method to create a clean testing setup."""
+        """Runs before every method to create a clean testing setup
+        """
         self.package = pysatMadrigal.instruments
 
     def teardown(self):
-        """Runs after every method to clean up previous testing."""
+        """Runs after every method to clean up previous testing
+        """
         del self.package
