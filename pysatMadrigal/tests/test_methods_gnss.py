5c7ecd472000721d690af7b0a8ed81b740d5b1de
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-09-15 12:15:59 -0400

    TST: added GNSS methods unit tests
    Added unit tests for the GNSS methods routines.

---
+++ pysatMadrigal/tests/test_methods_gnss.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Full license can be found in License.md
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.3824979
+# ----------------------------------------------------------------------------
+"""Test methods for `pysatMadrigal.instruments.methods.gnss`."""
+
+import logging
+import numpy as np
+import pytest
+
+import pysat
+
+from pysatMadrigal.instruments.methods import gnss
+
+
+class TestGNSSRefs():
+    """Test the acknowledgements and references for the JRO instruments."""
+
+    def setup(self):
+        """Run before every method to create a clean testing setup."""
+        self.out = None
+
+    def teardown(self):
+        """Run after every method to clean up previous testing."""
+        del self.out
+
+    @pytest.mark.parametrize("func, comp_str, in_args", [
+        ('acknowledgements', 'GPS TEC data products', ['tec']),
+        ('references', 'Rideout and Coster', ['tec', 'vtec'])])
+    def test_ref_output(self, func, comp_str, in_args):
+        """Test the GNSS acknowledgements and references."""
+        self.out = getattr(gnss, func)(*in_args)
+        assert self.out.find(comp_str) >= 0
+        return
