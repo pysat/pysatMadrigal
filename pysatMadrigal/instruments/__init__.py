ae8f0b28ccccb5298f2ea3053300a99e77cd0185
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-01-06 13:36:31 -0500

    ENH: updated instrument init
    Updated the instrument init by:
    - breaking out the imports to different lines and
    - adding the new instrument.

--- pysatMadrigal/instruments/__init__.py
+++ pysatMadrigal/instruments/__init__.py
@@ -1,8 +1,11 @@
 # Import Madrigal instruments
-from pysatMadrigal.instruments import dmsp_ivm, gnss_tec, jro_isr
+from pysatMadrigal.instruments import dmsp_ivm
+from pysatMadrigal.instruments import gnss_tec
+from pysatMadrigal.instruments import jro_isr
+from pysatMadrigal.instruments import madrigal_pandas
 
 # Import Madrigal methods
 from pysatMadrigal.instruments import methods  # noqa F401
 
 # Define variable name with all available instruments
-__all__ = ['dmsp_ivm', 'gnss_tec', 'jro_isr']
+__all__ = ['dmsp_ivm', 'gnss_tec', 'jro_isr', 'madrigal_pandas']
