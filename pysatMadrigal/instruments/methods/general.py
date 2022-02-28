975c2e1f526f44abca35f4cc9882143eeb90f038
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-02-28 13:58:40 -0500

    ENH: added madrigal file format function
    Added a function to return the known madrigal file formats and test these formats for pysat parsing compatibility.  Also updated docstrings and local comments.

--- pysatMadrigal/instruments/methods/general.py
+++ pysatMadrigal/instruments/methods/general.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# Full license can be found in License.md
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.3824979
+# ----------------------------------------------------------------------------
 # -*- coding: utf-8 -*-.
 """General routines for integrating CEDAR Madrigal instruments into pysat.
 
@@ -217,6 +222,266 @@ def known_madrigal_inst_codes(pandas_format=None):
         inst_codes = multi_dim
 
     return inst_codes
+
+
+def madrigal_file_format_str(inst_code, strict=False):
+    """Supplies known Madrigal instrument codes with a brief description.
+
+    Parameters
+    ----------
+    inst_code : int
+       Madrigal instrument code as an integer
+    strict : bool
+       If True, returns only file formats that will definitely not have a
+       problem being parsed by pysat.  If False, will return any file format.
+       (default=False)
+
+    Returns
+    -------
+    fstr : str
+        File formatting string that may or may not be parsable by pysat
+
+    Note
+    ----
+    File strings that have multiple '*' wildcards typically have several
+    experiment types and require a full pysat Instrument to properly manage
+    these types.
+
+    """
+
+    if not isinstance(inst_code, int):
+        inst_code = int(inst_code)
+
+    format_str = {
+        120: 'imf{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        210: 'geo{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        211: 'aei{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        212: 'dst{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        170: 'pfx{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        180: 'dmp{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        8100: 'dms*_{{year:04d}}{{month:02d}}{{day:02d}}_*.{{version:03d}}.',
+        8105: 'van_allen_{{year:04d}}_{{month:02d}}.{{version:03d}}.',
+        8400: '???{{year:04d}}{{month:02d}}{{day:02d}}j*.{{version:03d}}.',
+        8250: 'jic{{year:04d}}{{month:02d}}{{day:02d}}_mag.{{version:03d}}.',
+        8255: 'pmt*.',
+        8300: 'smt*.',
+        7800: 'gbt{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        10: 'jro{{year:04d}}{{month:02d}}{{day:02d}}*.{{version:03d}}.',
+        20: 'aro*{{year:02d}}{{month:02d}}{{day:02d}}a.{{version:03d}}.',
+        21: 'aro*{{year:02d}}{{month:02d}}{{day:02d}}*g.{{version:03d}}.',
+        22: 'ar?*{{year:02d}}{{month:02d}}{{day:02d}}*.{{version:03d}}.',
+        25: 'mui{{year:02d}}{{month:02d}}{{day:02d}}?.{{version:03d}}.',
+        30: 'mlh{{year:02d}}{{month:02d}}{{day:02d}}?.{{version:03d}}.',
+        31: 'mlh{{year:02d}}{{month:02d}}{{day:02d}}?.{{version:03d}}.',
+        32: 'mlh{{year:02d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        40: 'sts{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        41: 'sts{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        42: 'sts{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        43: 'sts{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        45: 'kha{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        50: 'cht{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        53: 'ist{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        57: 'mlv{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        61: 'pfa{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        70: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*.',
+        71: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@kir.',
+        72: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@uhf.',
+        73: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@sod.',
+        74: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@vhf.',
+        75: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@vkrv*.',
+        76: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@sdv*.',
+        80: 'son{{year:02d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        85: 'ALT{{year:02d}}{{month:02d}}{{day:02d}}_*.',
+        91: 'ran{{year:02d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        92: 'ras{{year:02d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        95: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@esr.',
+        100: 'MAD????_{year:04d}}-{{month:02d}}-{{day:02d}}_*@quj.',
+        310: 'gcm*.',
+        311: 'ami*.',
+        312: 'tdi*.',
+        320: 'sdt*.',
+        321: 'sdl*.',
+        322: 'gsw*.',
+        820: 'hhf*.',
+        830: 'syf*.',
+        845: 'khf*.',
+        861: 'shf*.',
+        870: 'gbf*.',
+        900: 'fhf*.',
+        910: 'whf*.',
+        911: 'ehf*.',
+        1040: 'arm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1140: 'pkr{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1180: 'ssr*.',
+        1210: 'sbf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1215: 'dav{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1220: 'maf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1221: 'rth{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1230: 'ccf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1240: 'adf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1245: 'rtg{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1254: 'tyr{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1270: 'kau{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1275: 'yam{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1285: 'plr{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1310: 'wak{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1320: 'cof{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1340: 'saf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1375: 'rpk{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1390: 'trf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1395: 'sym_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1400: 'hmf_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        13: 'D{{year:04d}}{{month:02d}}*.',
+        1539: 'asc{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1540: 'rmr_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1560: 'atm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1620: 'dum{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        1750: 'obn{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1775: 'emr{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        1780: 'wmr*.',
+        1781: 'mmr*.',
+        1782: 'bmr*.',
+        1783: 'smr*.',
+        1784: 'som{{year:02d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1785: 'amr{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1786: 'cpr_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1787: 'bpr_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1788: 'kgr_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        1790: 'ame*.',
+        1791: 'sco*.',
+        1792: 'lcm*.',
+        1793: 'alo{{year:04d}}{{month:02d}}{{day:02d}}_{{version:03d}}.',
+        2090: 'cia{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        2200: 'pla{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        2550: 'uld*.',
+        2890: 'ssd{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        2900: 'sdi*.',
+        2930: 'qad{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        2950: 'trd*.',
+        2951: 'lrd*.',
+        2952: 'krd*.',
+        5000: 'spf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5005: 'pfi{{year:04d}}{{month:02d}}{{day:02d}}.',
+        5015: 'ahf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5020: 'hfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5060: 'mjf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5140: 'aqf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5145: 'jfp{{year:04d}}{{month:02d}}{{day:02d}}_*.{{version:03d}}.',
+        5150: 'mfp{{year:04d}}{{month:02d}}{{day:02d}}_*.{{version:03d}}.',
+        5160: 'afp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5190: 'kha{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5240: 'fpf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5292: 'aaf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5300: 'pfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5340: 'mfp{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.?.',
+        5360: 'kfp{{year:02d}}{{month:02d}}{{day:02d}}g*.',
+        5370: 'aif{{year:02d}}{{month:02d}}{{day:02d}}g*.',
+        5380: 'clf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5430: 'wfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5460: 'cfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5465: 'pkf{{year:02d}}{{month:02d}}{{day:02d}}*.',
+        5470: 'FYU{{year:04d}}{{month:02d}}{{day:02d}}.',
+        5475: 'PKZ{{year:04d}}{{month:02d}}{{day:02d}}.',
+        5480: 'sfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5510: 'ikf{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5535: 'rfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5540: 'tfp{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5545: ''.join(['minime01_car_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5546: ''.join(['minime02_caj_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5547: ''.join(['minime06_par_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5548: ''.join(['minime02_uao_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5549: 'Kirtland Airforce Base FPI',
+        5550: ''.join(['minime09_vti_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5551: ''.join(['minime08_ann_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5552: 'Merihill Peru FPI',
+        5553: 'Nazca Peru FPI',
+        5554: ''.join(['minime07_euk_{{year:04d}}{{month:02d}}{{day:02d}}.',
+                       'cedar.{{version:03d}}.']),
+        5600: 'jbs_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        5700: 'spm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5720: 'dbm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5860: 'stm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5900: 'sfm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5950: 'rbm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        5980: 'eum{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        6205: 'akl{{year:02d}}{{month:02d}}{{day:02d}}g.*.',
+        6206: 'asl{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        6300: 'uil{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        6320: 'Colorado State sodium lidar',
+        6330: 'usl{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        6340: 'alo*.',
+        6350: 'nlo*.',
+        6360: 'cul*.',
+        6370: 'unl*.',
+        6380: 'pfl{{year:04d}}{{month:02d}}{{day:02d}}_{{cycle:03d}}.',
+        7190: 'usi*.',
+        7192: 'amp{{year:02d}}{{month:02d}}{{day:02d}}?.{{version:03d}}.',
+        7200: 'mhi{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7201: 'aai{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7202: 'abi{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7203: 'eai{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7204: 'mai{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7205: 'rai{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7206: 'jci{{year:04d}}{{month:02d}}{{day:02d}}.{kindat}.',
+        7240: 'mhi*.',
+        7580: 'qac*.',
+        11: 'j??*{{year:02d}}{{month:02d}}{{day:02d}}g.{{version:03d}}.',
+        840: 'jul{{year:04d}}{{month:02d}}{{day:02d}}_esf.{{version:03d}}.',
+        3000: 'utx*.',
+        8001: '????_?_??.gps_all.out.',
+        8000: '*{{year:02d}}{{month:02d}}{{day:02d}}*.{{version:03d}}.',
+        8002: '????_?_??.gps_all.out.',
+        8010: 'scin_{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        3010: 'dvs{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        3320: 'wup{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        4470: 'p4p{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        4473: 'y4p{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.',
+        4480: 'arp{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        4481: 'agp{{year:04d}}{{month:02d}}{{day:02d}}.{{version:03d}}.',
+        7191: 'mtm{{year:02d}}{{month:02d}}{{day:02d}}g.{kindat}.'}
+
+    # Warn if file format not available
+    msg = ""
+    if inst_code not in format_str.keys():
+        msg = "".join(["file format string not available for ",
+                       "instrument code {:d}: ".format(inst_code)])
+        fstr = "*."
+    else:
+        fstr = format_str[inst_code]
+
+    # Warn if file format has multiple '*' wildcards
+    num_wc = len(fstr.split("*"))
+    if num_wc >= 3:
+        msg = "".join(["file format string has multiple '*' ",
+                       "wildcards, may not be parsable by pysat"])
+    elif fstr.find('{{year') < 0 and fstr != "*.":
+        msg = "".join(["file format string missing date info, ",
+                       "may not be parsable by pysat"])
+    elif num_wc > 1:
+        nspec_sec = 0
+        for fsplit in fstr.split("*"):
+            if fsplit.find("}}") > 0 and fsplit.find("{{") >= 0:
+                nspec_sec += 1
+
+        if nspec_sec > 1:
+            msg = "".join(["file format string has '*' between formatting",
+                           " constraints, may not be parsable by pysat"])
+
+    if len(msg) > 0:
+        if strict:
+            raise ValueError(msg)
+        else:
+            logger.warning(msg)
+
+    fstr += "{file_type}"
+
+    return fstr
 
 
 def load(fnames, tag='', inst_id='', xarray_coords=None):
@@ -643,7 +908,7 @@ def download(date_array, inst_code=None, kindat=None, data_path=None,
     return
 
 
-def get_remote_filenames(inst_code=None, kindat=None, user=None, password=None,
+def get_remote_filenames(inst_code=None, kindat='', user=None, password=None,
                          web_data=None, url="http://cedar.openmadrigal.org",
                          start=dt.datetime(1900, 1, 1), stop=dt.datetime.now(),
                          date_array=None):
@@ -654,10 +919,10 @@ def get_remote_filenames(inst_code=None, kindat=None, user=None, password=None,
     inst_code : str or NoneType
         Madrigal instrument code(s), cast as a string.  If multiple are used,
         separate them with commas. (default=None)
-    kindat : str or NoneType
+    kindat : str
         Madrigal experiment code(s), cast as a string.  If multiple are used,
         separate them with commas.  If not supplied, all will be returned.
-        (default=None)
+        (default='')
     data_path : str or NoneType
         Path to directory to download data to. (default=None)
     user : str or NoneType
@@ -671,10 +936,12 @@ def get_remote_filenames(inst_code=None, kindat=None, user=None, password=None,
         (default=None)
     url : str
         URL for Madrigal site (default='http://cedar.openmadrigal.org')
-    start : dt.datetime
-        Starting time for file list (defaults to 01-01-1900)
-    stop : dt.datetime
-        Ending time for the file list (defaults to time of run)
+    start : dt.datetime or NoneType
+        Starting time for file list, None reverts to default
+        (default=dt.datetime(1900, 1, 1))
+    stop : dt.datetime or NoneType
+        Ending time for the file list, None reverts to default
+        (default=dt.datetime.utcnow())
     date_array : dt.datetime or NoneType
         Array of datetimes to download data for. The sequence of dates need not
         be contiguous and will be used instead of start and stop if supplied.
@@ -706,7 +973,7 @@ def get_remote_filenames(inst_code=None, kindat=None, user=None, password=None,
 
     _check_madrigal_params(inst_code=inst_code, user=user, password=password)
 
-    if kindat is None:
+    if kindat == '':
         kindat = []
     else:
         kindat = [int(kk) for kk in kindat.split(",")]
@@ -718,9 +985,18 @@ def get_remote_filenames(inst_code=None, kindat=None, user=None, password=None,
                 date_array))
         start = date_array.min()
         stop = date_array.max()
+
+    # If NoneType was supplied for start or stop, set to defaults
+    if start is None:
+        start = dt.datetime(1900, 1, 1)
+
+    if stop is None:
+        stop = dt.datetime.utcnow()
+
     # If start and stop are identical, increment
     if start == stop:
         stop += dt.timedelta(days=1)
+
     # Open connection to Madrigal
     if web_data is None:
         web_data = madrigalWeb.MadrigalData(url)
@@ -823,7 +1099,7 @@ def list_remote_files(tag, inst_id, inst_code=None, kindats=None, user=None,
         where the values file format template strings. (default=None)
     url : str
         URL for Madrigal site (default='http://cedar.openmadrigal.org')
-    two_digit_year_break : int
+    two_digit_year_break : int or NoneType
         If filenames only store two digits for the year, then
         '1900' will be added for years >= two_digit_year_break
         and '2000' will be added for years < two_digit_year_break.
@@ -897,10 +1173,10 @@ def list_remote_files(tag, inst_id, inst_code=None, kindats=None, user=None,
                                                       two_digit_year_break)
 
 
-def list_files(tag, inst_id, data_path=None, format_str=None,
+def list_files(tag, inst_id, data_path, format_str=None,
                supported_tags=None, file_cadence=dt.timedelta(days=1),
                two_digit_year_break=None, delimiter=None, file_type=None):
-    """Return a Pandas Series of every file for chosen Instrument data.
+    """Create a Pandas Series of every file for chosen Instrument data.
 
     Parameters
     ----------
@@ -910,9 +1186,8 @@ def list_files(tag, inst_id, data_path=None, format_str=None,
     inst_id : str
         Specifies the instrument ID to load. Accepts strings corresponding to
         the appropriate Madrigal Instrument `inst_ids`.
-    data_path : str or NoneType
-        Path to data directory.  If None is specified, the value previously
-        set in Instrument.files.data_path is used.  (default=None)
+    data_path : str
+        Path to data directory.
     format_str : str or NoneType
         User specified file format.  If None is specified, the default
         formats associated with the supplied tags are used. (default=None)
@@ -1048,7 +1323,7 @@ def _check_madrigal_params(inst_code, user, password):
 
     inst_codes = known_madrigal_inst_codes(None)
 
-    if inst_code not in inst_codes.keys():
+    if str(inst_code) not in inst_codes.keys():
         raise ValueError(''.join(["Unknown Madrigal instrument code: ",
                                   repr(inst_code), ". If this is a valid ",
                                   "Madrigal instrument code, please update ",
