462faba4a74b43b10a501f8d76534bd3e5f60c7d
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2023-03-20 14:11:48 -0400

    TST: added more DMSP unit tests
    Added unit tests for the ephemeris function and basic unit tests for the unit vector and polar cap velocity functions.

--- pysatMadrigal/tests/test_methods_dmsp.py
+++ pysatMadrigal/tests/test_methods_dmsp.py
@@ -5,11 +5,12 @@
 # ----------------------------------------------------------------------------
 """Test methods for `pysatMadrigal.instruments.methods.gnss`."""
 
+import logging
 import numpy as np
 import pytest
 
 import pysat
-from pysat.utils.testing import eval_bad_input
+from pysat.utils.testing import assert_lists_equal, eval_bad_input
 
 from pysatMadrigal.instruments.methods import dmsp
 
@@ -45,6 +46,7 @@ class TestDMSPCleaning(object):
         self.inst = pysat.Instrument('pysat', 'testing', use_header=True)
         self.inst.load(date=pysat.instruments.pysat_testing._test_dates[''][''])
         self.smooth_kwargs = {'rpa_vel_key': 'dummy4'}
+        self.inst['mlat'] = self.inst['latitude']
         return
 
     def teardown_method(self):
@@ -141,4 +143,126 @@ class TestDMSPCleaning(object):
         assert self.inst.meta[self.smooth_kwargs['rpa_vel_key'],
                               self.inst.meta.labels.desc].find(
                                   'RPA flag data <= ') >= 0
+        return
+
+    def test_no_ephem_logger(self, caplog):
+        """Test logger info raised without an ephemeris instrument."""
+
+        # Try to update the ephemeris without providing any ephemeris data
+        with caplog.at_level(logging.INFO, logger='pysat'):
+            dmsp.update_DMSP_ephemeris(self.inst)
+
+        # Test that there is no data
+        assert self.inst.empty, "Updating ephemeris w/o data didn't remove data"
+
+        # Test the logger message
+        assert caplog.text.find('No ephemera provided') >= 0
+        return
+
+    def test_no_ephem_data_logger(self, caplog):
+        """Test logger info raised without ephemeris data."""
+
+        ephem = pysat.Instrument('pysat', 'testing', use_header=True)
+        ephem.date = self.inst.date
+
+        # Try to update the ephemeris without providing any ephemeris data
+        with caplog.at_level(logging.INFO, logger='pysat'):
+            dmsp.update_DMSP_ephemeris(self.inst, ephem)
+
+        # Test that there is no data
+        assert self.inst.empty, "Updating ephemeris w/o data didn't remove data"
+
+        # Test the logger message
+        assert caplog.text.find('unable to load ephemera for') >= 0
+        return
+
+    def test_bad_ephem_inst_id(self):
+        """Test that mismatched `inst_id`s raises a ValueError."""
+
+        ephem = pysat.Instrument('pysat', 'testing', use_header=True)
+        ephem.inst_id = 'f15'
+
+        eval_bad_input(dmsp.update_DMSP_ephemeris, ValueError,
+                       "ephemera provided for the wrong satellite",
+                       input_args=[self.inst, ephem])
+
+        return
+
+    def test_good_ephem_local_load(self):
+        """Test that `update_DMSP_ephemeris` will load correct data."""
+
+        # Rename the testing instrument variables
+        def add_ephem_variables(ephem):
+            """Function to add required variables to a test instrument.
+
+            Parameters
+            ----------
+            ephem : pysat.Instrument
+                Instrument to be updated
+
+            """
+            ephem['SC_AACGM_LTIME'] = ephem['mlt'] + 1.0
+            ephem['SC_AACGM_LAT'] = ephem['latitude'] + 1.0
+            return
+
+        ephem = pysat.Instrument('pysat', 'testing', use_header=True)
+        ephem.custom_attach(add_ephem_variables)
+
+        # Adjust the Instrument ephemeris
+        dmsp.update_DMSP_ephemeris(self.inst, ephem)
+
+        # Test the output
+        assert_lists_equal(self.inst['mlt'].values,
+                           ephem['SC_AACGM_LTIME'].values)
+        assert_lists_equal(self.inst['mlat'].values,
+                           ephem['SC_AACGM_LAT'].values)
+
+        return
+
+    def test_add_drift_unit_vectors(self):
+        """Test that drift unit vectors are added."""
+
+        dmsp.add_drift_unit_vectors(self.inst)
+
+        # Ensure the variable are present
+        for new_var in ['unit_ram_x', 'unit_ram_y', 'unit_cross_x',
+                        'unit_cross_y', 'unit_ram_r', 'unit_ram_theta',
+                        'unit_cross_r', 'unit_cross_theta']:
+            assert new_var in self.inst.variables, "missing {:s}".format(
+                new_var)
+
+        # Ensure the variables behave like unit vectors
+        for direction in ['ram', 'cross']:
+            local_r = np.sqrt(self.inst['unit_{:s}_x'.format(direction)]**2
+                              + self.inst['unit_{:s}_y'.format(direction)]**2)
+            for unit_r in local_r.values:
+                if not np.isnan(unit_r):
+                    assert abs(unit_r - 1.0) <= 1.0e-7
+
+        return
+
+    @pytest.mark.parametrize("rpa_flag_key", ['int8_dummy', None])
+    def test_add_drifts_polar_cap_x_y(self, rpa_flag_key):
+        """Test that polar cap drifts are added.
+
+        Parameters
+        ----------
+        rpa_flag_key : str
+            Key for RPA flag
+
+        """
+
+        dmsp.add_drift_unit_vectors(self.inst)
+        dmsp.add_drifts_polar_cap_x_y(self.inst, rpa_flag_key=rpa_flag_key,
+                                      rpa_vel_key='dummy1',
+                                      cross_vel_key='dummy4')
+
+        # Ensure the variable are present
+        for new_var in ['unit_ram_x', 'unit_ram_y', 'unit_cross_x',
+                        'unit_cross_y', 'unit_ram_r', 'unit_ram_theta',
+                        'unit_cross_r', 'unit_cross_theta', 'ion_vel_pc_x',
+                        'ion_vel_pc_y', 'partial']:
+            assert new_var in self.inst.variables, "missing {:s}".format(
+                new_var)
+
         return
