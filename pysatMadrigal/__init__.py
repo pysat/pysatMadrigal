87fb3cacaeca62c9e19edc665c7094d020d8d416
jklenzing <jklenzing@users.noreply.github.com>
2023-05-30 13:29:19 -0400

    ENH: add version to package

--- pysatMadrigal/__init__.py
+++ pysatMadrigal/__init__.py
@@ -1,2 +1,10 @@
+import os
 from pysatMadrigal import instruments  # noqa F401
 from pysatMadrigal import utils  # noqa F401
+
+# set version
+here = os.path.abspath(os.path.dirname(__file__))
+version_filename = os.path.join(here, 'version.txt')
+with open(version_filename, 'r') as version_file:
+    __version__ = version_file.read().strip()
+del here, version_filename, version_file
