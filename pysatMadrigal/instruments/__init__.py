1b0e17e14ed9c20306495eed9fffade9d30b86fa
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2023-07-31 13:51:31 -0400

    STY: fixed import order
    Fixed the import order to be alphabetical.

--- pysatMadrigal/instruments/__init__.py
+++ pysatMadrigal/instruments/__init__.py
@@ -9,9 +9,9 @@ from pysatMadrigal.instruments import dmsp_ivm
 from pysatMadrigal.instruments import dmsp_ssj
 from pysatMadrigal.instruments import gnss_tec
 from pysatMadrigal.instruments import jro_isr
-from pysatMadrigal.instruments import madrigal_pandas
 from pysatMadrigal.instruments import madrigal_dst
 from pysatMadrigal.instruments import madrigal_geoind
+from pysatMadrigal.instruments import madrigal_pandas
 from pysatMadrigal.instruments import ngdc_ae
 from pysatMadrigal.instruments import omni2_imf
 
@@ -19,5 +19,5 @@ from pysatMadrigal.instruments import omni2_imf
 from pysatMadrigal.instruments import methods  # noqa F401
 
 # Define variable name with all available instruments
-__all__ = ['dmsp_ivm', 'dmsp_ssj', 'gnss_tec', 'jro_isr', 'madrigal_pandas',
-           'madrigal_dst', 'madrigal_geoind', 'ngdc_ae', 'omni2_imf']
+__all__ = ['dmsp_ivm', 'dmsp_ssj', 'gnss_tec', 'jro_isr', 'madrigal_dst',
+           'madrigal_geoind', 'madrigal_pandas', 'ngdc_ae', 'omni2_imf']
