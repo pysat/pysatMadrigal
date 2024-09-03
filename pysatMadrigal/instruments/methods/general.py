1ae098339d1d4d9b709e230744081681b68777c0
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2024-09-03 11:48:31 -0400

    BUG: remove timezone
    Remove unnecessary information from timestamps.

--- pysatMadrigal/instruments/methods/general.py
+++ pysatMadrigal/instruments/methods/general.py
@@ -1022,7 +1022,8 @@ def download(date_array, inst_code=None, kindat=None, data_path=None,
 
 def get_remote_filenames(inst_code=None, kindat='', user=None, password=None,
                          web_data=None, url="http://cedar.openmadrigal.org",
-                         start=dt.datetime(1900, 1, 1), stop=dt.datetime.now(),
+                         start=dt.datetime(1900, 1, 1),
+                         stop=dt.datetime.now(tz=dt.datetime.utc),
                          date_array=None):
     """Retrieve the remote filenames for a specified Madrigal experiment.
 
@@ -1103,6 +1104,7 @@ def get_remote_filenames(inst_code=None, kindat='', user=None, password=None,
 
     if stop is None:
         stop = dt.datetime.now(tz=dt.timezone.utc)
+    stop = pysat.utils.time.filter_datetime_input(stop)
 
     # If start and stop are identical, increment
     if start == stop:
